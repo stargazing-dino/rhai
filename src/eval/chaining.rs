5128f9049ea91a9d51f8094ac5c620881988e842
Stephen Chung <schungx@live.com>
2023-09-07 13:18:04 +0800

    Fix typo.

--- src/eval/chaining.rs
+++ src/eval/chaining.rs
@@ -20,14 +20,13 @@ static INDEXER_HASHES: OnceCell<(u64, u64)> = OnceCell::new();
 #[inline(always)]
 #[must_use]
 fn hash_idx() -> (u64, u64) {
-    *INDEXER_HASHES
-        .get_or_init(|| {
-            (
-                calc_fn_hash(None, FN_IDX_GET, 2),
-                calc_fn_hash(None, FN_IDX_SET, 3),
-            )
-        })
-        .into()
+    *INDEXER_HASHES.get_or_init(|| {
+        (
+            calc_fn_hash(None, FN_IDX_GET, 2),
+            calc_fn_hash(None, FN_IDX_SET, 3),
+        )
+            .into()
+    })
 }
 
 /// Method of chaining.
